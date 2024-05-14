38a3a828ee67ee07350e1fd1c9680a334afec2d5
SoundOfDed <ankud9900@gmail.com>
2024-05-14 23:14:33 +0300

    store developing

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import Image from "next/image";
+import { Expand, ShoppingCart } from "lucide-react";
 
 import { Product } from "@/types";
-import IconButton from "./icon-button";
+import IconButton from "@/components/ui/icon-button";
+import Currency from "@/components/ui/currency";
 
 interface ProductCard {
     data: Product;
@@ -22,11 +24,31 @@ const ProductCard: React.FC<ProductCard> = ({
                     alt="Image"
                     className="aspect-square object-cover rounded-md"
                 />
-                <div className="opacity-0 group-hover:opacity-100">
+                <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
-                        <IconButton />
+                        <IconButton 
+                            onClick={() => {}}
+                            icon={<Expand size={20} className="text-gray-600"/>}
+                        />
+                        <IconButton 
+                            onClick={() => {}}
+                            icon={<ShoppingCart size={20} className="text-gray-600"/>}
+                        />
                     </div>
                 </div>
+            </div>
+            {/* Description */}
+            <div>
+                <p className="font-semibold text-lg">
+                    {data.name}
+                </p>
+                <p className="text-sm text-gray-500">
+                    {data.category.name}
+                </p>
+            </div>
+            {/* Price */}
+            <div className="flex items-center justify-between">
+                <Currency value={data?.price}/>
             </div>
         </div>
     );
